1fa66daa230ad00c2eaffec57f9c40dedd2fdf95
LeshaSid <lesha.sidoruk@gmail.com>
2025-09-26 18:21:02 +0300

    humanize output in task_10.py

--- Lab1/task_10.py
+++ Lab1/task_10.py
@@ -3,6 +3,9 @@ b = int(input("Enter b: "))
 print(str(a) + " + " + str(b) + " = " + str(a+b))
 print(str(a) + " - " + str(b) + " = " + str(a-b))
 print(str(a) + " * " + str(b) + " = " + str(a*b))
-print(str(a) + " / " + str(b) + " = " + str(a/b))
-print(str(a) + " % " + str(b) + " = " + str(a%b))
-print(str(a) + " ** " + str(b) + " = " + str(a**b))
+if (b == 0):
+    print("На ноль делить нельзя!")
+else:
+    print(str(a) + " / " + str(b) + " = " + str(a/b))
+    print("Остаток от деления " + str(a) + " на " + str(b) + " = " + str(a%b))
+print(str(a) + " ^ " + str(b) + " = " + str(a**b))
