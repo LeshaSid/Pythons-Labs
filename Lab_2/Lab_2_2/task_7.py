31f5cebd4baf689a114b03d1cd8ba25eb4a4dba4
LeshaSid <lesha.sidoruk@gmail.com>
2025-11-04 23:11:20 +0300

    modified task_7

--- Lab_2/Lab_2_2/task_7.py
+++ Lab_2/Lab_2_2/task_7.py
@@ -1,30 +1,25 @@
-def merge_sorted_list(list1, list2):
-    merged_list = []
+def merge_sorted_ls(ls1, ls2):
+    merged_ls = []
     i = 0
     j = 0
     
-    while i < len(list1) and j < len(list2):
-        if list1[i] < list2[j]:
-            merged_list.append(list1[i])
+    while i < len(ls1) and j < len(ls2):
+        if ls1[i] <= ls2[j]:
+            merged_ls.append(ls1[i])
             i += 1
         else:
-            merged_list.append(list2[j])
+            merged_ls.append(ls2[j])
             j += 1
     
-    while i < len(list1):
-        merged_list.append(list1[i])
-        i += 1
+    merged_ls.extend(ls1[i:])
+    merged_ls.extend(ls2[j:])
     
-    while j < len(list2):
-        merged_list.append(list2[j])
-        j += 1
-    
-    return merged_list
+    return merged_ls
 
-list1 = [1, 3, 5, 7, 9]
-list2 = [0, 2, 4, 6, 8]
-print(merge_sorted_list(list1, list2))
+ls1 = [1, 3, 5, 7, 9]
+ls2 = [0, 2, 4, 6, 8]
+print(merge_sorted_ls(ls1, ls2))
 
-list1 = [0, 0, 0, 0, 0]
-list2 = [1, 1, 1, 1, 1]
-print(merge_sorted_list(list1, list2))
+ls1 = [0, 0, 0, 0, 0]
+ls2 = [1, 1, 1, 1, 1]
+print(merge_sorted_ls(ls1, ls2))
