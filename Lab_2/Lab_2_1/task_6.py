0005e0e4143d160ad7a67dbb5c79f1e8e718d0a2
LeshaSid <lesha.sidoruk@gmail.com>
2025-11-04 22:26:24 +0300

    modified task_6

--- Lab_2/Lab_2_1/task_6.py
+++ Lab_2/Lab_2_1/task_6.py
@@ -1,8 +1,9 @@
 string = input("Введите список через пробел: ")
 ls = string.split()
 
-unique_numbers = []
+unique_ls = []
 for n in ls:
-    if ls.count(n) == 1:
-        unique_numbers.append(n)
-print(f"Без дубликатов': {unique_numbers}")
+    if n not in unique_ls:
+        unique_ls.append(n)
+
+print(f"Без дубликатов: {unique_ls}")
