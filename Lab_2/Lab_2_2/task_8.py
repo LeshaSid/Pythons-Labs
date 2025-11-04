8f4a8198a1707871466324060cdb1f7f6c11963d
LeshaSid <lesha.sidoruk@gmail.com>
2025-11-04 23:25:59 +0300

    modified task_8

--- Lab_2/Lab_2_2/task_8.py
+++ Lab_2/Lab_2_2/task_8.py
@@ -1,17 +1,17 @@
 import time
 def timing(func):
     def wrapper(*args, **kwargs):
-        time_before = time.time()
+        time_before = time.perf_counter()
         result = func(*args, **kwargs)
-        time_after = time.time()
-        print(f"Время выполнения функции: {(time_after - time_before)*1000} мс")
+        time_after = time.perf_counter()
+        exe_time = (time_after - time_before)*1000
+        print(f"Время выполнения функции '{func.__name__}': {exe_time:.2f} мс")
         return result
     return wrapper
 
 @timing
 def f(name):
-    a = input("Press F to pay respect: ")
-    a.lower()
+    a = input("Press F to pay respect: ").lower()
     if (a == "f"):
         print(f"{name}, you paid respect!")
     else:
@@ -23,5 +23,16 @@ f("Алексей")
 def summmmmm(a, b):
     return a + b
 
-summmmmm(131543, 2326547665)
+summmmmm(13154657657658753454353, 232766576575676547665)
+summmmmm(1, 2)
+
+@timing
+def factorial(n):
+    fact = 1
+    for num in range(2, n + 1):
+        fact *= num
+    return fact
+    
+print(factorial(1211))
+
 
