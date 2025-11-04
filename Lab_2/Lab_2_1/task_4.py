39c42b72dec1656e9c42637c8ebb1100bae858d2
LeshaSid <lesha.sidoruk@gmail.com>
2025-11-04 22:15:50 +0300

    modified task_4

--- Lab_2/Lab_2_1/task_4.py
+++ Lab_2/Lab_2_1/task_4.py
@@ -1,35 +1,18 @@
-string = input("Введите 1-ый список чисел через пробел: ")
-ls_str = string.split()
-ls1 = []
-for n in ls_str:
-    ls1.append(int(n))
+ls_str_1 = input("Введите 1-ый список чисел через пробел: ")
+ls1 = [float(n) for n in ls_str_1.split()]
 
-string = input("Введите 2-ой список чисел через пробел: ")
-ls_str = string.split()
-ls2 = []
-for n in ls_str:
-    ls2.append(int(n))
+ls_str_2 = input("Введите 2-ой список чисел через пробел: ")
+ls2 = [float(n) for n in ls_str_2.split()]
 
-ls = ls1 + ls2
-both = []
-for n in ls:
-    if ls.count(n) > 1 and n not in both: 
-        both.append(n)
+set1 = set(ls1)
+set2 = set(ls2)
+both = set1 & set2
 print(f"Числа, которые присутствуют в обоих наборах одновременно: {both}")
 
-not_in_1 = []
-not_in_2 = []
-for n in ls1:
-    if n not in ls2:
-        not_in_2.append(n)
-for n in ls2:
-    if n not in ls1:
-        not_in_1.append(n)
+not_in_2 = set1 - set2
+not_in_1 = set2 - set1
 print(f"Числа из первого набора, которые отсутствуют во втором: {not_in_2}")
 print(f"Числа из второго набора, которые отсутствуют в первом: {not_in_1}")
 
-exceptions = []
-for n in ls:
-    if n not in both:
-        exceptions.append(n)
-print(f"Числа из обоих наборов, за исключением чисел, найденных в пункте 1: {not_in_1}")      
+exceptions = (set1 | set2) - set(both)
+print(f"Числа из обоих наборов, за исключением чисел, найденных в пункте 1: {exceptions}")      
