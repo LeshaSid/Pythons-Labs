4fa7b6b17e5aec32e6f9bd344a28a9193890609e
LeshaSid <lesha.sidoruk@gmail.com>
2025-11-04 22:49:35 +0300

    modified task_3

--- Lab_2/Lab_2_2/task_3.py
+++ Lab_2/Lab_2_2/task_3.py
@@ -10,8 +10,7 @@ def log_calls(filename):
 
 @log_calls("log_calls.txt")
 def f(name):
-    a = input("Press F to pay respect: ")
-    a.lower()
+    a = input("Press F to pay respect: ").lower()
     if (a == "f"):
         print(f"{name}, you paid respect!")
     else:
