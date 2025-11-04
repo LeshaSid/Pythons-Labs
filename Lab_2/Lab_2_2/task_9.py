1032be05640bb2da9b7df40375bd5a5cc2eba0b8
LeshaSid <lesha.sidoruk@gmail.com>
2025-11-04 23:34:49 +0300

    modified task_9

--- Lab_2/Lab_2_2/task_9.py
+++ Lab_2/Lab_2_2/task_9.py
@@ -1,8 +1,10 @@
 def type_check(type1, type2):
     def dec(func):
         def wrapper(*args, **kwargs):
-            if not isinstance(args[0], type1) or not isinstance(args[1], type2):
-                raise TypeError("Неверные типы!")
+            if len(args) >= 1 and not isinstance(args[0], type1):
+                raise TypeError(f"Неверные типы!")
+            if len(args) >= 2 and not isinstance(args[1], type2):
+                raise TypeError(f"Неверные типы!")
             return func(*args, **kwargs)
         return wrapper
     return dec
