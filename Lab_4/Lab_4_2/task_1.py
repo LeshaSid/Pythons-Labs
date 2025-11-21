e3183379d0560d00e15c3e2797a81e9b6ee1e52e
LeshaSid <lesha.sidoruk@gmail.com>
2025-11-21 11:48:16 +0300

    done task_1

--- Lab_4/Lab_4_2/task_1.py
+++ Lab_4/Lab_4_2/task_1.py
@@ -1,17 +1,25 @@
 import numpy as np 
 
-exps_trans = np.array([2500, 2400, 2300, 2200, 2100, 2000, 1900, 1800, 1700, 1600, 1500, 3000])
-
-def analyze_transport_expenses(exp_arr):
+def analyze_transport_expenses(exps_arr):
     winter_idx = [11, 0, 1]
     summer_idx = [5, 6, 7]
 
-    sum_winter = np.sum(exp_arr[winter_idx])
-    sum_summer = np.sum(exp_arr[summer_idx])
+    sum_winter = np.sum(exps_arr[winter_idx])
+    sum_summer = np.sum(exps_arr[summer_idx])
 
     if sum_winter > sum_summer:
         print("Расходы в зимний период больше, чем в летний")
     elif sum_winter < sum_summer:
         print("Расходы в летний период больше, чем в зимний")
     else:
-        print("Расходы за зиминий и летний период равны")
+        print("Расходы за зимний и летний период равны")
+
+    print("Топ 3 месяца по расходам:")
+    top_indices = np.argsort(exps_arr)[::-1][:3]
+
+    for i in range(3):
+        idx = top_indices[i]
+        print(f"{i+1} место: {idx + 1} месяц с расходом {exps_arr[idx]}")
+
+exps_trans = np.array([2500, 2400, 2300, 2200, 2100, 2000, 1900, 1800, 1700, 1600, 1500, 3000])
+analyze_transport_expenses(exps_trans)
